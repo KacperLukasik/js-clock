6b2fd0c3a994d19be01ed8760312f6b537df2b72
KacperLukasik <ukasikpl1@outlook.com>
2014-09-15 23:24:05 +0200

    fix

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@ function clockTimer()
 {
 	var date = new Date();
 	
-	var time = [date.getHours(),date.getMinutes(),date.getSeconds()];
+	var time = [date.getHours(),date.getMinutes(),date.getSeconds()]; // |[0] = Hours| |[1] = Minutes| |[2] = Seconds|
 	
 	if(time[0] < 10){time[0] = "0"+ time[0];}
 	if(time[1] < 10){time[1] = "0"+ time[1];}
