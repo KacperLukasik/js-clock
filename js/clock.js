3005f7f6fc0cfb79ddb78cf355ac1230ed538711
KacperLukasik <ukasikpl1@outlook.com>
2014-09-15 22:59:05 +0200

    bugfix2

--- js/clock.js
+++ js/clock.js
@@ -3,14 +3,11 @@ function clockTimer()
 	var date = new Date();
 	
 	var time = [date.getHours(),date.getMinutes(),date.getSeconds()];
-<<<<<<< HEAD
 	
 	if(time[0] < 10){time[0] = "0"+ time[0];}
 	if(time[1] < 10){time[1] = "0"+ time[1];}
 	if(time[2] < 10){time[2] = "0"+ time[2];}
 	
-=======
->>>>>>> origin/master
 	var current_time = [time[0],time[1],time[2]].join(':');
 	var clock = document.getElementById("clock");
 	
